774cc72a30fcb14753796f953c65f4274e89b673
Kacian417 <knicho4492@gmail.com>
2024-10-06 00:50:09 +0000

    practice

--- src/app.js
+++ src/app.js
@@ -7,5 +7,13 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const element = document.querySelectorAll(".demo1");
+  element.innerHTML = "This is a test.";
+  console.log(element);
+  element[0].classList.remove("alert-warning");
+  console.log(element[0].classList);
+
+  //for (let i = 0; i < element.length; i++) {
+  // console.log(element[i].childNodes[0].data);
+  // }
 };
